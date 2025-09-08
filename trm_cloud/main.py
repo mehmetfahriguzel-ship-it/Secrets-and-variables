702bad178e91c5c905856ebe210a26e25c4c6470
mehmetfahriguzel-ship-it <mehmetfahriguzel@gmail.com>
2025-09-08 19:31:08 +0300

    Update main.py

--- trm_cloud/main.py
+++ trm_cloud/main.py
@@ -1,8 +1,1 @@
-print("TRM Cloud Automation çalışıyor 🚀")
-
-try:
-    # Test için küçük bir kod
-    x = 5 / 1
-    print("Kod başarıyla çalıştı:", x)
-except Exception as e:
-    print("Hata oluştu:", e)
+print("TRM Cloud Automation başarıyla çalıştı 🚀")
